8023096f41b94d5c29f5d6cfa3ddb7c02c30f78b
Luis Rangel <luisrangelc@gmail.com>
2020-12-29 19:46:06 -0600

    add login and register

---
+++ lib/theme/routes.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:torre_test2/views/login_screen.dart';
+// import 'package:torre_test2/views/menu.dart';
+import 'package:torre_test2/views/register_screen.dart';
+
+class AppRoutes {
+  AppRoutes._();
+
+  static const String authLogin = '/auth-login';
+  static const String authRegister = '/auth-register';
+  static const String menu = '/menu';
+
+  static Map<String, WidgetBuilder> define() {
+    return {
+      authLogin: (context) => Login(),
+      authRegister: (context) => Register(),
+      // menu: (context) => MenuScreen(),
+    };
+  }
+}
