e2a977cf5f49ce48d8ade43037abc4ea3ee0bf18
Luis Rangel <luisrangelc@gmail.com>
2020-12-30 15:34:53 -0600

    finally runs on iOS :)

--- lib/views/jobs_screen.dart
+++ lib/views/jobs_screen.dart
@@ -2,6 +2,7 @@ import 'dart:convert' as convert;
 import 'package:flutter/material.dart';
 import 'package:torre_test2/models/job_model.dart';
 import 'package:torre_test2/net/API.dart';
+import 'package:torre_test2/utils/size_config.dart';
 
 class JobsScreen extends StatefulWidget {
   @override
@@ -39,21 +40,49 @@ class _JobsScreenState extends State<JobsScreen> {
   @override
   build(context) {
     return Scaffold(
-      // appBar: AppBar(
-      //   title: Text("Jobs List"),
-      // ),
+      appBar: AppBar(
+        title: Text("Jobs List"),
+        automaticallyImplyLeading: false,
+      ),
       body: Column(
         children: [
           Text('Hi Friend!!!'),
-          ListView.builder(
-            itemCount: jobs.length,
-            itemBuilder: (context, index) {
-              return ListTile(
-                title: Text(
-                  jobs[index].objective,
-                ),
-              );
-            },
+          RaisedButton(
+            onPressed: () {},
+            color: Theme.of(context).accentColor,
+            child: Padding(
+              padding: EdgeInsets.fromLTRB(SizeConfig.safeBlockHorizontal * 5,
+                  0, SizeConfig.safeBlockHorizontal * 5, 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    'Continue',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w700,
+                      color: Colors.black,
+                    ),
+                  ),
+                  Icon(
+                    Icons.arrow_forward,
+                    color: Colors.white,
+                  )
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: jobs.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(
+                    jobs[index].objective,
+                  ),
+                );
+              },
+            ),
           ),
         ],
       ),
